55d1a3fd7cfea276a318872feb8c7749f6644b15
M SAI NITHIN <99406663+Nithin6524@users.noreply.github.com>
2023-12-28 23:20:13 +0530

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,23 +1,33 @@
-import React from "react"
-export default function Home(){
-    return(
-        <div>
-            <h1 className="home-title">Better Notes</h1>
-            <div classname="units">
-                
-                    <img className="os-img" src="./OS.png"/>
-                
-                <img className="cn-img" src="./CN.png"/>
-                
-                     <img className="laa-img" src="./LAA.png" />
-                
-                
-                    <img className="daa-img" src="./DAA.png"/>
-                
-                
-                    <img className="mpca-img" src="./MPCA.png"/>
-             
-            </div>
-        </div>
+import React,{useState} from "react"
+
+export default class Home extends React.Component{
+constructor(){
+ super();
+    this.state={
+        isDark:false,
+        img:"./light.png" 
+    }
+}
+handler=()=>{
+    this.setState(prevState=>{
+    return{
+        img:prevState.img==="./light.png"?"./dark.png":"./light.png" 
+    }
+    }
     )
+}
+
+    render(){
+    return(
+        <div style={{
+            backgroundImage: `url(${this.state.img})`,
+        
+            width: "1350px",
+            height: "1000px"
+        }}>
+        
+            <button onClick={this.handler}>change</button>
+       </div>
+    )
+}
 }
