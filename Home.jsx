3240508f646bb374571eeb7e8baa8927b5a7b630
M SAI NITHIN <99406663+Nithin6524@users.noreply.github.com>
2023-12-28 12:53:21 +0530

    Create Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+export default function Home(){
+    return(
+        <div>
+            <h1 className="home-title">Better Notes</h1>
+            <div classname="units">
+                
+                    <img className="os-img" src="./OS.png"/>
+                
+                <img className="cn-img" src="./CN.png"/>
+                
+                     <img className="laa-img" src="./LAA.png" />
+                
+                
+                    <img className="daa-img" src="./DAA.png"/>
+                
+                
+                    <img className="mpca-img" src="./MPCA.png"/>
+             
+            </div>
+        </div>
+    )
+}
