c23e5372147dc00d0cbe6176e1ce039a44a20bdc
M SAI NITHIN <99406663+Nithin6524@users.noreply.github.com>
2023-12-28 13:43:58 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Providers>
-      <App />
-    </Providers>
-  </React.StrictMode>
+
+ReactDOM.createRoot(document.getElementById("root") ).render(
+    <App />
 );
